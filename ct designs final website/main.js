f36f126cb90e8b97eaee2a8347164bbab489c26c
linus mate <mlynux.com>
2022-07-06 17:35:54 +0300

    web files

--- ct designs final website/main.js
+++ ct designs final website/main.js
@@ -1,6 +1,6 @@
 
 
-const search = () =>{
+const search = () => {
   const searchbox = document.getElementById("search-item").value.toUpperCase();
   const storeitems = document.getElementById("bags-collection")
   const product = document.querySelectorAll(".js")
@@ -10,7 +10,7 @@ const search = () =>{
     let match = product [i].getElementByTagName('h2')[0];
 
     if(match){
-      let textvalue = match.textContent || match.innerHTML
+      let textvalue = match.textContent || match.innerHTML;
 
       if(textvalue.toUpperCase().indexOf(searchbox) > -1){
         product[i].style.display = "";
@@ -20,3 +20,39 @@ const search = () =>{
     }
   }
 }
+
+window.onload = function() {
+  const pass_field = document.getElementById("pass-key");
+  const showBtn = document.getElementById("passShow");
+  showBtn.addEventListener("click", function(){
+    if (pass_field.type === "password"){
+      pass_field.type = "text";
+      showBtn.innerHTML = "hide";
+      // showBtn.style.color = "3498db";
+    }else{
+      pass_field.type = "password";
+      showBtn.innerHTML = "show";
+      // showBtn.style.color = "#222";
+  }
+})
+}
+
+
+/*$(document).ready(function(){
+ const pass_field = document.getElementById("pass-key");
+const showBtn = document.getElementById("mm");
+console.log(showBtn)
+console.log("heu")
+showBtn.addEventListener("click", function(){
+  console.log("alert")
+  if (pass_field.type === "password"){
+    pass_field.type = "text";
+    showBtn.textContent = "hide";
+    showBtn.style.color = "3498db";
+  }else{
+    pass_field.type = "password";
+    showBtn.textContent = "show";
+    showBtn.style.color = "#222";
+  }
+})
+}) */
